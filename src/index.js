199c83b7060a4bbb531cec2116701c340e82e6d8
Felipe Alves <felipe.alves@efabrika.com.br>
2022-08-17 17:58:41 -0300

    Add google analytics

--- src/index.js
+++ src/index.js
@@ -10,9 +10,13 @@ import 'font-awesome/css/font-awesome.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
+import ReactGA from 'react-ga';
 
 fontawesome.library.add(brands, solid, regular)
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactGA.initialize('UA-146556270-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 registerServiceWorker();
